af8c2741e2ff79f47b57c3a96b065d72e71eb829
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-06-13 13:38:25 +0900

    Allow to copy the signature prototype file on creating a new RBS file

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { Uri } from 'vscode';
+import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('open-rbs-file.openRbsFile', openRBSFile));
@@ -12,8 +13,9 @@ function openRBSFile(): void {
 	const currentFile = vscode.window.activeTextEditor?.document.uri;
 	if (currentFile) {
 		if (currentFile.path.endsWith('.rb')) {
-			const rbsFileUri = getRBSFileUri(currentFile);
-			ensureFile(rbsFileUri).then(() => {
+			const rbsFileUri = getSignatureFileUri(currentFile);
+			const signaturePrototypeFileUri = getSignaturePrototypeFileUri(currentFile);
+			ensureFile(rbsFileUri, signaturePrototypeFileUri).then(() => {
 				vscode.commands.executeCommand('vscode.open', rbsFileUri)
 			});
 		} else if (currentFile.path.endsWith('.rbs')) {
@@ -23,23 +25,35 @@ function openRBSFile(): void {
 	}
 }
 
-async function ensureFile(uri: Uri): Promise<void> {
-	const baseDirectory = Uri.joinPath(uri, '..');
+async function ensureFile(targetUri: Uri, templateUri: Uri): Promise<void> {
+	const baseDirectory = Uri.joinPath(targetUri, '..');
 	await vscode.workspace.fs.createDirectory(baseDirectory);
-	try {
-		await vscode.workspace.fs.stat(uri)
-	} catch {
-		const workspaceEdit = new vscode.WorkspaceEdit();
-		workspaceEdit.createFile(uri);
-		vscode.workspace.applyEdit(workspaceEdit);
-	};
+	if (!fs.existsSync(targetUri.path)) {
+		if (isCopySignaturePrototypeOnCreate() && fs.existsSync(templateUri.path)) {
+			await vscode.workspace.fs.copy(templateUri, targetUri);
+		} else {
+			const workspaceEdit = new vscode.WorkspaceEdit();
+			workspaceEdit.createFile(targetUri);
+			vscode.workspace.applyEdit(workspaceEdit);
+		}
+	}
 }
 
-function getRBSFileUri(rubyFileUri: Uri): Uri {
+function getSignatureFileUri(rubyFileUri: Uri): Uri {
 	const workspaceFolder = vscode.workspace.workspaceFolders?.find((workspaceFolder) => rubyFileUri.path.startsWith(workspaceFolder.uri.path));
 	if (workspaceFolder) {
 		const relativeRubyFilePath = rubyFileUri.path.replace(workspaceFolder.uri.path, '');
 		return Uri.file(`${workspaceFolder.uri.path}${getSignatureDirectory()}${relativeRubyFilePath}s`);
+	} else {
+		return rubyFileUri;
+	}
+}
+
+function getSignaturePrototypeFileUri(rubyFileUri: Uri): Uri {
+	const workspaceFolder = vscode.workspace.workspaceFolders?.find((workspaceFolder) => rubyFileUri.path.startsWith(workspaceFolder.uri.path));
+	if (workspaceFolder) {
+		const relativeRubyFilePath = rubyFileUri.path.replace(workspaceFolder.uri.path, '');
+		return Uri.file(`${workspaceFolder.uri.path}${getSignaturePrototypeDirectory()}${relativeRubyFilePath}s`);
 	} else {
 		return rubyFileUri;
 	}
@@ -63,4 +77,17 @@ function getSignatureDirectory(): string {
 	} else {
 		return `/${directory}`;
 	}
+}
+
+function getSignaturePrototypeDirectory(): string {
+	const directory = vscode.workspace.getConfiguration('open-rbs-file').get('signature-prototype-directory') as string;
+	if (directory.startsWith('/')) {
+		return directory;
+	} else {
+		return `/${directory}`;
+	}
+}
+
+function isCopySignaturePrototypeOnCreate(): boolean {
+	return vscode.workspace.getConfiguration('open-rbs-file').get('copy-signature-prototype-on-create') as boolean;
 }
